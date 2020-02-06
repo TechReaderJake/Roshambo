831d83ab25e9ffe87c9f44fae8826403004747f7
Jacob Ashcraft <00151399@badgermail.snow.edu>
2020-02-06 12:13:39 -0700

    Working on a grid/card for the main style flow.

--- clients/yugen.reactjs/src/app/components/control-center.js
+++ clients/yugen.reactjs/src/app/components/control-center.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function ControlCenter() {
     return (
-        <div id="control-center">
+        <div id="control-center" className="content">
             <ul>
                 <li>Login | Register</li>
                 <li><img src="https://via.placeholder.com/40x40?text=LG" alt="temp logo" /></li>
